270e65f957af2521f7928c2daf231ef18bada3c1
John Montejano <johnmontejano@Johns-MacBook-Pro-2.local>
2019-09-01 18:42:54 -0700

    README initialized

---
+++ libs.py
@@ -0,0 +1,12 @@
+word_list = list()
+verb = input("Type in a verb: ")
+word_list.append(verb)
+
+noun = input("Type in a noun: ")
+word_list.append(noun)
+
+pronoun = input("Type in a pronoun: ")
+word_list.append(pronoun)
+
+adjective = input("Type in an adjective: ")
+word_list.append(adjective)
