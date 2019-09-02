16545a298699c6c6161cde7850ff7f55c10b448d
John Montejano <johnmontejano@Johns-MacBook-Pro-2.local>
2019-09-01 18:55:59 -0700

    Colors are imported. To be used to output users input into a color in terminal

--- libs.py
+++ libs.py
@@ -1,3 +1,6 @@
+from __future__ import print_function  # so works on Python 2 and 3 alike
+from colors import red, green, blue, yellow
+
 word_list = list()
 
 verb = input("Type in a verb: ")
@@ -13,4 +16,4 @@ adjective = input("Type in an adjective: ")
 word_list.append(adjective)
 
 print("I ate some {} then I went to {} I didn't realize I ate {} I didn't realize I ate {} of them. I like playing the".format(
-    verb, noun, pronoun, adjective))
+    red(verb), green(noun), blue(pronoun), yellow(adjective)))
