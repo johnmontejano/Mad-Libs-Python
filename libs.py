b0586e80f309dc0c858f05788c064e6675635f4f
John Montejano <johnmontejano@Johns-MacBook-Pro-2.local>
2019-09-02 21:21:41 -0700

    [add]show results prompt

--- libs.py
+++ libs.py
@@ -1,27 +1,34 @@
-from __future__ import print_function  # so works on Python 2 and 3 alike
+from __future__ import print_function # so works on Python 2 and 3 alike
 from colors import red, green, blue, yellow
-
+word_list = list()
 
 def libs():
-    word_list = list()
+  verb = input("Type in a verb: ")
+  word_list.append(verb)
 
-    verb = input("Type in a verb: ")
-    word_list.append(verb)
+  noun = input("Type in a noun: ")
+  word_list.append(noun)
 
-    noun = input("Type in a noun: ")
-    word_list.append(noun)
+  pronoun = input("Type in a pronoun: ")
+  word_list.append(pronoun)
 
-    pronoun = input("Type in a pronoun: ")
-    word_list.append(pronoun)
-
-    adjective = input("Type in an adjective: ")
-    word_list.append(adjective)
-
-    print("I ate some {} then I went to {} I didn't realize I ate {} I didn't realize I ate {} of them. I like playing the".format(
-        red(verb), green(noun), blue(pronoun), yellow(adjective)))
-
-    print('Your results:')
-    print(word_list)
+  adjective = input("Type in an adjective: ")
+  word_list.append(adjective)
 
 
+  print(red('verb:red'))
+  print(green('noun:green'))
+  print(blue('pronoun:blue'))
+  print(yellow('adjective:yellow'))
+
+
+  print("I {} some {} then I've seen {}. I didn't realize it's {}" .format(red(verb), green(noun),blue(pronoun), yellow(adjective)))
+  
+  inputs = input('Enter R to show results: ')
+
+  if inputs == 'r' or inputs == 'R':
+    for list_item in word_list:
+      print(list_item)
+   
+    
 libs()
